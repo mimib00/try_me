8fe178427cd309fdb9668c5642154a0e0917da66
Mohamed Malkia <mmimib001@gmail.com>
2022-09-29 13:14:49 +0100

    added assets

--- lib/views/root/root.dart
+++ lib/views/root/root.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:try_me/meta/utils/try_me_icons_icons.dart';
 
 class RootScreen extends StatelessWidget {
   const RootScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Icon(TryMeIcons.calander),
+      ),
+    );
   }
 }

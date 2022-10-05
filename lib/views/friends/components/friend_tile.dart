42cdf5a50349ce8b8773ba4ad6de55225afd24f6
Mohamed Malkia <mmimib001@gmail.com>
2022-10-05 12:21:44 +0100

    display friends outfit list

--- lib/views/friends/components/friend_tile.dart
+++ lib/views/friends/components/friend_tile.dart
@@ -1,7 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/firestore.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
+import 'package:try_me/meta/models/post.dart';
 import 'package:try_me/meta/models/user.dart';
 import 'package:try_me/meta/utils/constants.dart';
 import 'package:try_me/meta/widgets/post_card.dart';
@@ -17,7 +20,7 @@ class FriendTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -55,17 +58,39 @@ class FriendTile extends StatelessWidget {
               ],
             ),
           ),
+          const SizedBox(height: 10),
           SizedBox(
             width: 100.w,
             height: 300,
-            child: ListView.builder(
-              padding: const EdgeInsets.only(bottom: 10),
-              physics: const BouncingScrollPhysics(),
+            child: FirestoreListView(
               scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Container();
-                // return PostCard(user: user);
+              pageSize: 5,
+              query: FirebaseFirestore.instance
+                  .collection("posts")
+                  .where("owner", isEqualTo: user.uid)
+                  .orderBy("created_at", descending: true)
+                  .withConverter(
+                    fromFirestore: (snapshot, options) async {
+                      final userSnap =
+                          await FirebaseFirestore.instance.collection("users").doc(snapshot.data()!["owner"]).get();
+                      final user = Users.fromJson(userSnap.data()!, userSnap.id);
+                      return Post.fromJson(
+                        snapshot.data()!,
+                        snapshot.id,
+                        user,
+                      );
+                    },
+                    toFirestore: (value, options) => {},
+                  ),
+              itemBuilder: (context, doc) {
+                return FutureBuilder<Post>(
+                  future: doc.data(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) return Container();
+                    final post = snapshot.data!;
+                    return PostCard(post: post);
+                  },
+                );
               },
             ),
           ),
